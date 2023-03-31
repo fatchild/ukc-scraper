58ef48b71e8a45cead13e0b7267f391d60bccb51
EdwardOakley <47759261+fatchild@users.noreply.github.com>
2023-03-31 10:04:10 +0100

    Parallel requests

--- ukc-scraper.sh
+++ ukc-scraper.sh
@@ -1,18 +1,31 @@
 #! /bin/bash
 
 url="https://www.ukclimbing.com/logbook/crags/crag-1/route-"
-request_delay=0
+request_delay=2
+concurrent_requests=10
 
 # Headers
-echo "url, output (match)"
+echo "url"
 
-for ((n=480620;n<6890000;n++))
-do
-    output=$(wget -qO- "$url$n" | grep -iE "Dan Varian|dan varian|d varian|dan v|D. Varian.|D Varian|Dan V|Dan v|dan V|dan v")
-    match=${#output}
-    if [ $match -gt 0 ]
+
+match_on_name () {
+    output=$(wget -qO- "$url$1")
+    match_faist=$(echo $output | grep -iE "Dan Varian|dan varian|d varian|dan v|D. Varian.|D Varian|Dan V|Dan v|dan V|dan v")
+    match_length=${#match_faist}
+    if [ $match_length -gt 0 ]
     then
-        echo $url$n, $match
+        echo $url$1
     fi
+}
+
+
+for ((n=480620;n<6890000;n += $concurrent_requests))
+do
+    for ((x=0;x<$concurrent_requests;x++))
+    do
+        # echo "Adding request" $x $(expr $n + $x)
+        match_on_name "$(expr $n + $x)" &
+    done
+    wait
     sleep $request_delay
 done
